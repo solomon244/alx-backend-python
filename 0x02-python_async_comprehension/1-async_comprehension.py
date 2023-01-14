27d7143af0b06e5185f9f093b74861c762cf4c2b
solomon244 <slegesse2444@gmail.com>
2023-01-14 04:58:31 +0300

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-'''collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.
-'''
+"""
+collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.
+"""
+import asyncio
+import random
 from typing import List
-from importlib import import_module as using
 
-
-async_generator = using('0-async_generator').async_generator
+async_generator = __import__("0-async_generator").async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number generator.
-    '''
-    return [num async for num in async_generator()]
+    """
+    Creates a list of 10 numbers from a 10-number generator.
+    """
+    return [n async for n in async_generator()]
